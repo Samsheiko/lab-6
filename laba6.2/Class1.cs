e61febbe2f855689e240f4848c467626df1c1c65
Samsheiko <sheiko22@mail.ru>
2026-06-29 21:03:41 +0500

    Add files via upload

---
+++ laba6.2/Class1.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // 1. Вводим данные с клавиатуры
+        Console.Write("Введите x: ");
+        double inputX = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите y: ");
+        double inputY = Convert.ToDouble(Console.ReadLine());
+
+        // 2. Создаем объект класса Point
+        Point myPoint = new Point(inputX, inputY);
+
+        // 3. Вызываем метод и выводим результат
+        double distance = myPoint.CalculateDistance();
+        Console.WriteLine($"Расстояние до начала координат: {distance}");
+    }
+}
---
+++ laba6.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Point
+{
+    public double x;
+    public double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double CalculateDistance()
+    {
+        return Math.Sqrt(x * x + y * y);
+    }
+}
