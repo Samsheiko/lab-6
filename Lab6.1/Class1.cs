ee4b0693ad6fd0aab4b4a754c2c4311a47f48909
Samsheiko <sheiko22@mail.ru>
2026-06-29 21:17:15 +0500

    Add files via upload

---
+++ Lab6.1/Class1.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // 1. Создаем машину
+        Car myCar = new Car("Toyota", 10.5, 7.2, 55.0);
+        Console.WriteLine(myCar.ToString());
+
+        // 2. Проверяем конструктор копирования
+        FuelInfo copy = new FuelInfo(myCar);
+        Console.WriteLine("Копия параметров: " + copy.ToString());
+
+        // 3. Округляем значения
+        myCar.ToInteger();
+        Console.WriteLine("После округления: " + myCar.ToString());
+
+        // 4. Работа методов
+        Console.WriteLine($"Запас хода на трассе: {myCar.MaxHighwayDistance()} км");
+        Console.WriteLine($"Средний расход: {myCar.AverageRate()} л/100км");
+    }
+}
---
+++ Lab6.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+// Базовый класс: Параметры расхода
+public class FuelInfo
+{
+    public double CityRate;    // Расход в городе
+    public double HighwayRate; // Расход на трассе
+    public double TankVolume;  // Объем бака
+
+    // Конструктор
+    public FuelInfo(double city, double highway, double tank)
+    {
+        CityRate = city;
+        HighwayRate = highway;
+        TankVolume = tank;
+    }
+
+    // Конструктор копирования
+    public FuelInfo(FuelInfo other)
+    {
+        this.CityRate = other.CityRate;
+        this.HighwayRate = other.HighwayRate;
+        this.TankVolume = other.TankVolume;
+    }
+
+    // Метод приведения к целому типу
+    public void ToInteger()
+    {
+        CityRate = (int)CityRate;
+        HighwayRate = (int)HighwayRate;
+        TankVolume = (int)TankVolume;
+    }
+
+    public override string ToString()
+    {
+        return $"Расход: город {CityRate}, трасса {HighwayRate}. Бак: {TankVolume} л";
+    }
+}
+
+// Дочерний класс: Марка машины
+public class Car : FuelInfo
+{
+    public string Brand;
+
+    public Car(string brand, double city, double highway, double tank)
+        : base(city, highway, tank)
+    {
+        Brand = brand;
+    }
+
+    // Метод 1: Сколько км проедет по трассе на полном баке
+    public double MaxHighwayDistance() => (TankVolume / HighwayRate) * 100;
+
+    // Метод 2: Средний расход (смешанный цикл)
+    public double AverageRate() => (CityRate + HighwayRate) / 2;
+
+    public override string ToString()
+    {
+        return $"Марка: {Brand}. " + base.ToString();
+    }
+}
+
+
